7e885487dd1bec3b4d3a96e7af1839ecaef8943d
EternalDesp <petrunin.vitya2015@yandex.ru>
2024-09-24 20:44:27 +0300

    Create #laba1.py

---
+++ #laba1.py
@@ -0,0 +1,6 @@
+#laba1
+import pandas as pd
+import numpy as np
+DATA_URL = "https://raw.githubusercontent.com/Yorko/mlcourse.ai/main/data/"
+df = pd.read_csv(DATA_URL + "adult.data.csv")
+df.head()
